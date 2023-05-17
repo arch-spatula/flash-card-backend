05e5d0ac1f67dbe623f2b77eae68681bceb9a3c1
arch-spatula <shkim516@naver.com>
2023-05-17 17:18:08 +0900

    test: remove add function to main_test #1

--- main_test.ts
+++ main_test.ts
@@ -1,5 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.187.0/testing/asserts.ts";
-import { add } from "./main.ts";
+
+const add = (a: number, b: number) => a + b;
 
 Deno.test(function addTest() {
   assertEquals(add(2, 3), 5);
