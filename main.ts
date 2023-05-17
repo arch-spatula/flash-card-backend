6895cd42072fae362879cce2d9956cf5df8bace6
arch-spatula <shkim516@naver.com>
2023-05-17 16:28:08 +0900

    chore: install oak #1

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { Application, Router } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const app = new Application();
+const router = new Router();
+
+router
+  .get("/", (ctx) => {
+    ctx.response.body = "hello world!";
+  })
+  .get("/foo", (ctx) => {
+    ctx.response.body = "hello foo!";
+  })
+  .get("/bar", (ctx) => {
+    ctx.response.body = "hello bar!";
+  })
+  .get("/number/:id", (ctx) => {
+    const { id } = ctx.params;
+    ctx.response.body = `hello ${id}`;
+  })
+  .get("/string/:id", (ctx) => {
+    const { id } = ctx.params;
+    ctx.response.body = `hello ${id}`;
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
