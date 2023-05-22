ea65cf6e4eeee01bce4bfc11cf3480a00a964d76
arch-spatula <shkim516@naver.com>
2023-05-22 18:11:31 +0900

    refactor: add reading to id, question, answer #3

--- model/cards.ts
+++ model/cards.ts
@@ -1,14 +1,49 @@
-class Card {
+class CardRecord {
+  private _id?: string;
   private question: string;
-  private id: string;
   private answer: string;
   private submitDate: Date;
-  constructor(question: string, id: string, answer: string, date: Date) {
+  private stackCount: number;
+  private userId: string;
+  constructor(
+    question: string,
+    answer: string,
+    date: Date,
+    stackCount: number,
+    userId: string,
+    _id?: string
+  ) {
+    this._id = _id;
     this.question = question;
-    this.id = id;
     this.answer = answer;
     this.submitDate = date;
+    this.stackCount = stackCount;
+    this.userId = userId;
+  }
+  get getId() {
+    return this._id;
+  }
+  get getQuestion() {
+    return this.question;
+  }
+  get getAnswer() {
+    return this.answer;
+  }
+  get getSubmitDate() {
+    return this.submitDate;
+  }
+  get getStackCount() {
+    return this.stackCount;
   }
 }
 
-export default Card;
+// const card = new CardRecord(
+//   "CPU의 본딧말",
+//   "Central Processing Unit",
+//   new Date(),
+//   0,
+//   "1",
+//   "0"
+// );
+
+export default CardRecord;
