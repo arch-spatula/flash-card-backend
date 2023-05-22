530d7aab615c247df6716a367e457516b1fe9e28
arch-spatula <shkim516@naver.com>
2023-05-22 17:03:12 +0900

    refactor: axios card typing and testing #3

--- api/axios.ts
+++ api/axios.ts
@@ -4,13 +4,23 @@ import axios from "https://deno.land/x/redaxios@0.5.1/mod.ts";
 // axios 싱글튼
 const { APP_ID, CARD_API_KEY } = config();
 const baseURL = `https://us-west-2.aws.data.mongodb-api.com/app/${APP_ID}/endpoint/data/v1/action`; // /action
-const DATA_SOURCE = "Cluster0";
-const DATABASE = "cards_db";
-const COLLECTION = "cards";
 
+type Card = {
+  question: string;
+  answer: string;
+  submitDate: string;
+  stackCount: number;
+};
 class AxiosAPI {
   private static instance: AxiosAPI;
   private axiosConfig;
+  private query: {
+    dataSource: string;
+    database: string;
+    collection: string;
+    document?: Card;
+  };
+
   private constructor() {
     this.axiosConfig = axios.create({
       baseURL,
@@ -18,22 +28,60 @@ class AxiosAPI {
         "Content-Type": "application/json",
         "api-key": CARD_API_KEY,
       },
+      method: "POST",
     });
+    this.query = {
+      dataSource: "Cluster0",
+      database: "cards_db",
+      collection: "cards",
+    };
   }
-  public static getInstance(): AxiosAPI {
+
+  static getInstance(): AxiosAPI {
     if (!AxiosAPI.instance) {
       AxiosAPI.instance = new AxiosAPI();
     }
     return AxiosAPI.instance;
   }
+
   async getCards() {
-    this.axiosConfig.post("");
+    const body = JSON.stringify({});
+    // {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     "api-key": "qLnkJigiiCtNuBy7XmOOHTR6ruuYxtK8PnBhL85i3hFtQ3hKlGXGqvXHl3PbR43u"
+    //   },
+    //   body: '{"collection":"cards","database":"cards_db","dataSource":"Cluster0"}'
+    // }
+    return await this.axiosConfig.post("/find", {
+      body: JSON.stringify({
+        dataSource: "Cluster0",
+        database: "cards_db",
+        collection: "cards",
+      }),
+    });
+    // document: "",
+    // return await this.axiosConfig.get("/find");
   }
-  async postCards() {}
+
+  async postCards(payload: Card) {
+    this.query.document = payload;
+    return await this.axiosConfig.post("/insertOne", this.query);
+  }
+
   async patchCards() {}
   async deleteCards() {}
   async singin() {}
   async singup() {}
+}
+
+const axiosLog = AxiosAPI.getInstance();
+
+try {
+  console.log(await axiosLog.getCards());
+} catch (error) {
+  console.log(error);
 }
 
 export default AxiosAPI;
