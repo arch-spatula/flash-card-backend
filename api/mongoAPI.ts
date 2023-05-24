f076b6d54e821439cfe712c8aab5c949b3483c23
arch-spatula <shkim516@naver.com>
2023-05-24 19:27:30 +0900

    feat: 로그인, 회원가입 통신 함수 구현 #3

--- api/mongoAPI.ts
+++ api/mongoAPI.ts
@@ -8,6 +8,12 @@ import {
 
 const { APP_ID, CARD_API_KEY } = config();
 
+type Collection = {
+  dataSource: string;
+  database: string;
+  collection: string;
+};
+
 class MongoAPI {
   private static instance: MongoAPI;
   private baseURL: string;
@@ -16,11 +22,8 @@ class MongoAPI {
     headers: { "Content-Type": string; "api-key": string };
     body: BodyInit;
   };
-  private cardBody: {
-    dataSource: string;
-    database: string;
-    collection: "cards" | "user";
-  };
+  private cardBody: Collection;
+  private userBody: Collection;
   private constructor() {
     this.baseURL = `https://us-west-2.aws.data.mongodb-api.com/app/${APP_ID}/endpoint/data/v1/action`;
     this.options = {
@@ -36,6 +39,11 @@ class MongoAPI {
       database: "cards_db",
       collection: "cards",
     };
+    this.userBody = {
+      dataSource: "Cluster0",
+      database: "cards_db",
+      collection: "user",
+    };
   }
 
   static getInstance(): MongoAPI {
@@ -46,73 +54,138 @@ class MongoAPI {
   }
 
   async getCards() {
-    return await fetch(`${this.baseURL}/find`, this.options);
+    try {
+      const result = await fetch(`${this.baseURL}/find`, {
+        ...this.options,
+        body: JSON.stringify({ ...this.cardBody, filter: { userId: "1" } }),
+      });
+      return result.json();
+    } catch (error) {
+      return error;
+    }
   }
 
   async postCards(document: CardRecord) {
-    return await fetch(`${this.baseURL}/insertOne`, {
-      ...this.options,
-      body: JSON.stringify({
-        ...this.cardBody,
-        document,
-      }),
-    });
+    try {
+      const result = await fetch(`${this.baseURL}/insertOne`, {
+        ...this.options,
+        body: JSON.stringify({
+          ...this.cardBody,
+          document,
+        }),
+      });
+      return result.json();
+    } catch (error) {
+      return error;
+    }
   }
 
   async patchCards(document: CardRecord) {
-    const {
-      getQuestion: question,
-      getAnswer: answer,
-      getStackCount: stackCount,
-      getSubmitDate: submitDate,
-      getId: $oid,
-    } = document;
-
-    return await fetch(`${this.baseURL}/updateOne`, {
-      ...this.options,
-      body: JSON.stringify({
-        ...this.cardBody,
-        filter: { _id: { $oid } },
-        update: {
-          $set: {
-            question,
-            answer,
-            stackCount,
-            submitDate,
+    try {
+      const {
+        getQuestion: question,
+        getAnswer: answer,
+        getStackCount: stackCount,
+        getSubmitDate: submitDate,
+        getId: $oid,
+      } = document;
+      const result = await fetch(`${this.baseURL}/updateOne`, {
+        ...this.options,
+        body: JSON.stringify({
+          ...this.cardBody,
+          filter: { _id: { $oid } },
+          update: {
+            $set: {
+              question,
+              answer,
+              stackCount,
+              submitDate,
+            },
           },
-        },
-      }),
-    });
+        }),
+      });
+      return result.json();
+    } catch (error) {
+      return error;
+    }
   }
 
   async deleteCards($oid: string) {
-    return await fetch(`${this.baseURL}/deleteOne`, {
-      ...this.options,
-      body: JSON.stringify({
-        ...this.cardBody,
-        filter: { _id: { $oid } },
-      }),
-    });
+    try {
+      const result = await fetch(`${this.baseURL}/deleteOne`, {
+        ...this.options,
+        body: JSON.stringify({
+          ...this.cardBody,
+          filter: { _id: { $oid } },
+        }),
+      });
+      return result.json();
+    } catch (error) {
+      return error;
+    }
   }
 
-  async singup({ email, password }: { email: string; password: string }) {
-    //
-    // const salt = await genSalt(8);
-    // const passwordHash = await hash(password, salt);
-    const passwordHash = await hash(password);
-    const result = compare(password, passwordHash);
-    return result;
+  async signup({ email, password }: { email: string; password: string }) {
+    try {
+      if ((await this.getUser(email)) === null) {
+        return await this.postUser(email, password);
+      } else {
+        throw Error("이미 가입한 아이디입니다.");
+      }
+    } catch (error) {
+      return error;
+    }
   }
-  async singin() {}
-}
 
-const mongoAPI = MongoAPI.getInstance();
+  async signin({ email, password }: { email: string; password: string }) {
+    try {
+      const document = await this.getUser(email);
+      if (document === null) {
+        throw Error("이메일이 없습니다.");
+      } else {
+        if (!(await compare(password, document.passwordHash))) {
+          throw Error("비밀번호가 알치하지 않습니다.");
+        } else {
+          return document;
+        }
+      }
+    } catch (error) {
+      return error;
+    }
+  }
 
-try {
-  const user = { email: "user@email.com", password: "1234test" };
-  console.log(await mongoAPI.singup(user));
-} catch (error) {
-  console.log(error);
+  private async postUser(email: string, password: string) {
+    try {
+      const passwordSalt = await genSalt(8);
+      const passwordHash = await hash(password, passwordSalt);
+      const result = await fetch(`${this.baseURL}/insertOne`, {
+        ...this.options,
+        body: JSON.stringify({
+          ...this.userBody,
+          document: { email, passwordHash, passwordSalt },
+        }),
+      });
+      return result.json();
+    } catch (error) {
+      return error;
+    }
+  }
+
+  private async getUser(email: string) {
+    try {
+      const result = await fetch(`${this.baseURL}/findOne`, {
+        ...this.options,
+        body: JSON.stringify({
+          ...this.userBody,
+          filter: { email },
+        }),
+      });
+      const data = await result.json();
+      return data.document;
+    } catch (error) {
+      return error;
+    }
+  }
 }
 
 export default MongoAPI;
