238058503b4a284f0016afcf2921c6feb9c5715e
arch-spatula <shkim516@naver.com>
2023-05-25 09:38:23 +0900

    feat: 회원가입, 로그인 구현 #3

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,19 +1,42 @@
 import type { Context } from "https://deno.land/x/oak@v12.4.0/mod.ts";
+import MongoAPI from "../api/mongoAPI.ts";
+
+const mongoAPI = MongoAPI.getInstance();
 
 async function signup({ request, response }: Context) {
   try {
-    const body = await request.body().value;
-    //
+    if (!request.hasBody) {
+      throw Error("body가 없습니다.");
+    }
+    const { email, password } = await request.body().value;
+    if (!email || !password) {
+      throw Error("이메일 혹은 비밀 번호가 없습니다.");
+    }
     response.status = 201;
-    response.body = "ok";
-    // console.log(body.email);
+    response.body = await mongoAPI.signup({ email, password });
   } catch (error) {
-    console.log(error);
+    response.body = {
+      success: false,
+      msg: `${error}`,
+    };
   }
 }
 
-async function signin({ request, response }: Context) {
-  const body = 0;
+async function signin({ request, response, cookies }: Context) {
+  try {
+    if (!request.hasBody) {
+      throw Error("body가 없습니다.");
+    }
+    const { email, password } = await request.body().value;
+    response.status = 201;
+    response.body = await mongoAPI.signin({ email, password });
+    cookies.set("user", "1234");
+  } catch (error) {
+    response.body = {
+      success: false,
+      msg: `${error}`,
+    };
+  }
 }
 
 export { signup, signin };
