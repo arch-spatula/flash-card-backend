fedf9e831baf8b390019a2197e1c7f9e7b8c13ec
arch-spatula <shkim516@naver.com>
2023-05-25 23:23:51 +0900

    test: 통신만 하는 기능을 테스트에 반영 #3

--- api/MongoAPI.test.ts
+++ api/MongoAPI.test.ts
@@ -1,33 +1,38 @@
-import { assertEquals } from "https://deno.land/std@0.188.0/testing/asserts.ts";
-import { it, beforeAll } from "https://deno.land/std@0.188.0/testing/bdd.ts";
+import {
+  assertEquals,
+  assert,
+} from "https://deno.land/std@0.188.0/testing/asserts.ts";
 import CardRecord from "../model/cards.ts";
 import MongoAPI from "./mongoAPI.ts";
+import { beforeEach } from "https://deno.land/std@0.188.0/testing/bdd.ts";
 
 Deno.test("MongoAPI", () => {
   let mongoAPI: MongoAPI;
 
-  beforeAll(() => {
+  beforeEach(() => {
     mongoAPI = MongoAPI.getInstance();
   });
 
-  it("getCards should fetch cards from the database", async () => {
-    const response = await mongoAPI.getCards();
-    // const data = await response.json();
+  Deno.test("getCards should fetch cards from the database", async () => {
+    const response = await mongoAPI.getCards("user123");
+    const data = await response.json();
 
     assertEquals(response.status, 200);
+    assert(Array.isArray(data));
     // 여기에서 데이터에 대한 추가 검증을 수행할 수 있습니다.
   });
 
-  it("postCards should insert a card into the database", async () => {
+  Deno.test("postCards should insert a card into the database", async () => {
     const card = new CardRecord("Question", "Answer", new Date(), 0, "user123");
     const response = await mongoAPI.postCards(card);
-    // const data = await response.json();
+    const data = await response.json();
 
     assertEquals(response.status, 200);
+    assert(data.getOwnPropertyNames("insertedId"));
     // 여기에서 데이터에 대한 추가 검증을 수행할 수 있습니다.
   });
 
-  it("patchCards should update a card in the database", async () => {
+  Deno.test("patchCards should update a card in the database", async () => {
     const card = new CardRecord(
       "Updated Question",
       "Updated Answer",
@@ -37,43 +42,44 @@ Deno.test("MongoAPI", () => {
       "card123"
     );
     const response = await mongoAPI.patchCards(card);
-    // const data = await response.json();
+    const data = await response.json();
 
     assertEquals(response.status, 200);
+    assert(data.getOwnPropertyNames("modifiedCount"));
     // 여기에서 데이터에 대한 추가 검증을 수행할 수 있습니다.
   });
 
-  it("deleteCards should delete a card from the database", async () => {
+  Deno.test("deleteCards should delete a card from the database", async () => {
     const cardId = "card123";
     const response = await mongoAPI.deleteCards(cardId);
-    // const data = await response.json();
+    const data = await response.json();
 
     assertEquals(response.status, 200);
+    assert(data.getOwnPropertyNames("deletedCount"));
     // 여기에서 데이터에 대한 추가 검증을 수행할 수 있습니다.
   });
 
-  it("deleteCards should delete a card from the database", async () => {
-    const cardId = "card123";
-    const response = await mongoAPI.deleteCards(cardId);
-    assertEquals(response.status, 200);
-    // 여기에서 데이터에 대한 추가 검증을 수행할 수 있습니다.
-  });
-
-  it("getUser should fetch user from the database", async () => {
+  Deno.test("getUser should fetch user from the database", async () => {
     const email = "user@example.com";
     const response = await mongoAPI.getUser(email);
+    const data = await response.json();
+
     assertEquals(response.status, 200);
+    assert(data.getOwnPropertyNames("email"));
     // 데이터에 대한 추가 검증을 수행할 수 있습니다.
   });
 
-  it("postUser should insert a user into the database", async () => {
+  Deno.test("postUser should insert a user into the database", async () => {
     const user = {
       email: "user@example.com",
       passwordHash: "hashedPassword",
       passwordSalt: "salt",
     };
     const response = await mongoAPI.postUser(user);
+    const data = await response.json();
+
     assertEquals(response.status, 200);
+    assert(data.getOwnPropertyNames("insertedId"));
     // 데이터에 대한 추가 검증을 수행할 수 있습니다.
   });
 });
