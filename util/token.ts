bb1ed596d03130672370765f294199130b065cae
arch-spatula <shkim516@naver.com>
2023-05-25 17:16:36 +0900

    refactor: Token을 제어하는 클래스를 싱글튼으로 처리 #3

--- util/token.ts
+++ util/token.ts
@@ -1,27 +1,49 @@
-import { create, getNumericDate } from "https://deno.land/x/djwt@v2.8/mod.ts";
+import {
+  create,
+  getNumericDate,
+  verify,
+} from "https://deno.land/x/djwt@v2.8/mod.ts";
 
-async function makeKey() {
-  const key = await crypto.subtle.generateKey(
-    { name: "HMAC", hash: { name: "SHA-512" } },
-    true,
-    ["sign", "verify"]
-  );
-  return key;
+class Token {
+  private static instance: Token;
+  readonly key: Promise<CryptoKey>;
+
+  private constructor() {
+    this.key = (async () => {
+      const key = await crypto.subtle.generateKey(
+        { name: "HMAC", hash: { name: "SHA-512" } },
+        true,
+        ["sign", "verify"]
+      );
+      return key;
+    })();
+  }
+
+  static getInstance(): Token {
+    if (!Token.instance) {
+      Token.instance = new Token();
+    }
+    return Token.instance;
+  }
+
+  async makeToken(userId: string, sec = 3600) {
+    const jwt = await create(
+      { alg: "HS512" },
+      { exp: getNumericDate(sec), sub: userId },
+      await this.key
+    );
+    return {
+      jwt,
+      expires: {
+        expires: new Date(new Date().getTime() + sec * 1000),
+      },
+    };
+  }
+
+  async tokenToUserId(jwt: string) {
+    const { sub } = await verify(jwt, await this.key);
+    return sub;
+  }
 }
 
-async function makeToken(userId: string, sec = 3600) {
-  const key = await makeKey();
-  const jwt = await create(
-    { alg: "HS512" },
-    { exp: getNumericDate(sec), sub: userId },
-    key
-  );
-  return {
-    jwt,
-    expires: {
-      expires: new Date(new Date().getTime() + sec * 1000),
-    },
-  };
-}
-
-export { makeToken, makeKey };
+export { Token };
