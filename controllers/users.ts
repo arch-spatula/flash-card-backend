6349d0217ea694a1dd3a46144333f2902ece5b14
arch-spatula <shkim516@naver.com>
2023-05-25 14:35:58 +0900

    refactor: 인허가 API에 로직을 컨트롤러로 이동 #3

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,5 +1,10 @@
 import type { Context } from "https://deno.land/x/oak@v12.4.0/mod.ts";
 import MongoAPI from "../api/mongoAPI.ts";
+import {
+  hash,
+  genSalt,
+  compare,
+} from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
 
 const mongoAPI = MongoAPI.getInstance();
 
@@ -8,13 +13,29 @@ async function signup({ request, response }: Context) {
     if (!request.hasBody) {
       throw Error("body가 없습니다.");
     }
-    const { email, password } = await request.body().value;
-    if (!email || !password) {
+    const card = await request.body().value;
+
+    if (!card.email || !card.password) {
       throw Error("이메일 혹은 비밀 번호가 없습니다.");
     }
-    response.status = 201;
-    response.body = await mongoAPI.signup({ email, password });
+
+    const document = await mongoAPI.getUser(card.email);
+
+    if (document !== null) {
+      throw Error("이미 가입한 아이디입니다.");
+    } else {
+      const passwordSalt = await genSalt(8);
+      const passwordHash = await hash(card.password, passwordSalt);
+
+      response.status = 201;
+      response.body = await mongoAPI.postUser({
+        email: card.email,
+        passwordHash,
+        passwordSalt,
+      });
+    }
   } catch (error) {
+    response.status = 400;
     response.body = {
       success: false,
       msg: `${error}`,
@@ -27,11 +48,23 @@ async function signin({ request, response, cookies }: Context) {
     if (!request.hasBody) {
       throw Error("body가 없습니다.");
     }
-    const { email, password } = await request.body().value;
-    response.status = 201;
-    response.body = await mongoAPI.signin({ email, password });
-    cookies.set("user", "1234");
+    const card = await request.body().value;
+    if (!card.email || !card.password) {
+      throw Error("이메일 혹은 비밀 번호가 없습니다.");
+    }
+    const document = await mongoAPI.getUser(card.email);
+    if (!document) throw Error("이메일이 없습니다.");
+    else {
+      if (await compare(card.password, document.passwordHash)) {
+        response.status = 201;
+        response.body = document;
+        cookies.set("user", document._id);
+      } else {
+        throw Error("비밀번호가 알치하지 않습니다.");
+      }
+    }
   } catch (error) {
+    response.status = 400;
     response.body = {
       success: false,
       msg: `${error}`,
