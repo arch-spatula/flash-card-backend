a2419fb80e9021a7c8d21e0aaf93641b724e39b2
arch-spatula <shkim516@naver.com>
2023-05-27 17:04:02 +0900

    refactor: 임시 허용 라우팅 삭제 #5

--- routes/index.ts
+++ routes/index.ts
@@ -6,25 +6,15 @@ import {
   updateCard,
 } from "../controllers/cards.ts";
 import { signin, signup } from "../controllers/users.ts";
-import MongoAPI from "../api/mongoAPI.ts";
 
 const router = new Router();
 
 router
-  .get("/api", (ctx) => {
-    ctx.response.body = "hello world!";
-  })
   .get("/api/card", getCards)
   .post("/api/card", addCard)
   .patch("/api/card/:id", updateCard)
   .delete("/api/card/:id", deleteCard)
   .post("/api/auth/signup", signup)
-  .post("/api/auth/signin", signin)
-  .get("/api/secret", async (ctx) => {
-    const mongoAPI = MongoAPI.getInstance();
-
-    ctx.response.status = 200;
-    ctx.response.body = await mongoAPI.getAllCards();
-  });
+  .post("/api/auth/signin", signin);
 
 export default router;
