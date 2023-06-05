4f6e33eb6e7e1c46501f2f12f22bcb0b49917de4
arch-spatula <shkim516@naver.com>
2023-06-05 19:41:10 +0900

    refactor: remove token class #25

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -1,30 +1,59 @@
-import type { Context, Middleware } from '../deps.ts';
-import Token from '../util/token.ts';
+import type { Middleware } from '../deps.ts';
+import { convertTokenToUserId, refreshAccessToken } from '../util/token.ts';
 
-const token = Token.getInstance();
+class BadRequestError extends Error {}
+class AuthorizationError extends Error {}
 
 /**
  * @see https://github.com/gitdagray/mern_stack_course/blob/main/lesson_13-backend/middleware/verifyJWT.js
  */
 const authMiddleware: Middleware = async (
-  { request, response, cookies },
+  { request, response, cookies, state },
   next
 ) => {
-  const accessToken = request.headers.get('Authorization');
-  if (accessToken) {
-    await next();
-  } else {
-    const refreshToken = await cookies.get('user');
-    if (!refreshToken) {
-      response.status = 400;
-      response.body = {
-        msg: '로그아웃 되었습니다.',
-      };
-    } else {
+  try {
+    const refreshToken = request.headers.get('Authorization');
+    const accessToken = await cookies.get('user');
+    if (!refreshToken || !refreshToken.startsWith('Bearer ') || !accessToken)
+      throw new BadRequestError('Bad Request');
+
+    const userId = await convertTokenToUserId(accessToken);
+    if (!userId) {
+      const isNotExpired = await convertTokenToUserId(refreshToken);
+      if (!isNotExpired) {
+        throw new AuthorizationError('Unauthorized');
+      }
+
+      const { accessToken } = await refreshAccessToken(refreshToken);
       response.status = 401;
       response.body = {
-        msg: '토큰이 만료되었습니다.',
-        accessToken: token.refreshAccessToken(refreshToken),
+        success: false,
+        mag: 'new token is required',
+        access_token: accessToken,
+      };
+      return;
+    }
+
+    state.userId = userId;
+    await next();
+  } catch (error) {
+    if (error instanceof BadRequestError) {
+      response.status = 400;
+      response.body = {
+        success: false,
+        msg: `${error}`,
+      };
+    } else if (error instanceof AuthorizationError) {
+      response.status = 401;
+      response.body = {
+        success: false,
+        msg: `${error}`,
+      };
+    } else {
+      response.status = 406;
+      response.body = {
+        success: false,
+        msg: `${error}`,
       };
     }
   }
