08e5800ca35ea6d248f34d1b4238a32b36915516
arch-spatula <shkim516@naver.com>
2023-06-05 19:41:45 +0900

    test: Hold testing for new authMiddleWere #25

--- middleware/authMiddleware.test.ts
+++ middleware/authMiddleware.test.ts
@@ -1,67 +1,112 @@
-import { assertEquals, assert } from '../deps.ts';
+import { assertEquals } from '../deps.ts';
 import { testing } from '../deps.ts';
+
 import { authMiddleware } from './authMiddleware.ts';
 
 /**
  * @see https://github.com/oakserver/oak/pull/422/files#diff-724220dfa022a67c646170cb3066895fd7cda6e16a6d4d4c98a1e17aca7bde06
  * oak testing에서 cookie는 set으로 작성하고 get으로 읽을 수 없습니다.
+ *
+ * 라이브러리 신뢰할 수 없기 때문에 테스트를 보류합니다.
  */
 
-Deno.test({
-  name: 'auth Middleware access token과 refresh token 모두가 유효한 경우',
-  async fn() {
-    const ctx = testing.createMockContext({
-      headers: [['Authorization', 'accessToken']],
-    });
-    ctx.cookies.set('user', 'refreshToken');
-    const next = testing.createMockNext();
-
-    await authMiddleware(ctx, next);
-
-    assert(ctx.cookies.get('user'), 'refreshToken');
-    assert(ctx.request.headers.get('Authorization'), 'accessToken');
-    assertEquals(ctx.response.body, undefined);
-  },
+Deno.test('should not pass', () => {
+  const ctx = testing.createMockContext({
+    headers: [['Authorization', '']],
+  });
 });
 
-Deno.test({
-  name: 'access token이 만료 refresh token은 유효',
-  async fn() {
-    const refreshCookie = 'refreshToken';
-    const ctx = testing.createMockContext({
-      headers: [
-        ['Authorization', ''],
-        ['cookie', `user=${refreshCookie}`],
-      ],
-    });
-    const next = testing.createMockNext();
+Deno.test('should refresh token by setting cookie', () => {});
 
-    await authMiddleware(ctx, next);
+Deno.test('should pass and call next function', () => {});
 
-    assert(ctx.response.status === 401, '401');
-    assertEquals(
-      ctx.response.body,
-      {
-        msg: '토큰이 만료되었습니다.',
-        accessToken: Promise.resolve({ accessToken: null, success: false }),
-      },
-      'access token 갱신 응답'
-    );
-  },
-});
+// Deno.test(
+//   'authMiddleware should set userId header and call next() when both refresh token and access token are present',
+//   async () => {
+//     let nextCalled = false;
+//     const next = async () => {
+//       nextCalled = true;
+//     };
+//     const userId = 'mockUserId';
+//     const ctx = testing.createMockContext({
+//       headers: [['Authorization', '']],
+//     });
 
-Deno.test({
-  name: '모든 Token이 만료',
-  async fn() {
-    const ctx = testing.createMockContext({
-      headers: [['Authorization', '']],
-    });
-    await ctx.cookies.set('user', null);
-    const next = testing.createMockNext();
+//     await authMiddleware(ctx, next);
 
-    await authMiddleware(ctx, next);
+//     assertEquals(ctx.request.headers.get('userId'), userId);
+//     assertEquals(nextCalled, true);
+//   }
+// );
 
-    assert(ctx.response.status === 400, '400');
-    assertEquals(ctx.response.body, { msg: '로그아웃 되었습니다.' });
-  },
-});
+// Deno.test(
+//   'authMiddleware should return an error response when refreshToken is missing',
+//   async () => {
+//     const request = {
+//       headers: new Headers(),
+//     };
+//     const response = {};
+//     const cookies = {
+//       get: async () => 'accessToken',
+//     };
+
+//     await authMiddleware({ request, response, cookies }, () => {});
+
+//     assertEquals(response.status, 400);
+//     assertEquals(response.body, {
+//       success: false,
+//       msg: 'Bad Request',
+//     });
+//   }
+// );
+
+// Deno.test(
+//   'authMiddleware should return an error response when refreshToken is not in Bearer format',
+//   async () => {
+//     const request = {
+//       headers: new Headers({
+//         Authorization: 'InvalidFormat',
+//       }),
+//     };
+//     const response = {};
+//     const cookies = {
+//       get: async () => 'accessToken',
+//     };
+
+//     await authMiddleware({ request, response, cookies }, () => {});
+
+//     assertEquals(response.status, 400);
+//     assertEquals(response.body, {
+//       success: false,
+//       msg: 'Bad Request',
+//     });
+//   }
+// );
+
+// Deno.test(
+//   'authMiddleware should return an error response when accessToken is missing',
+//   async () => {
+//     const request = {
+//       headers: new Headers({
+//         Authorization: 'Bearer refreshToken',
+//       }),
+//     };
+//     const response = {};
+//     const cookies = {
+//       get: async () => null,
+//     };
+
+//     await authMiddleware({ request, response, cookies }, () => {});
+
+//     assertEquals(response.status, 400);
+//     assertEquals(response.body, {
+//       success: false,
+//       msg: 'Bad Request',
+//     });
+//   }
+// );
+
+// Deno.test(
+//   'authMiddleware should return an error response when accessToken is invalid',
+//   async () => {}
+// );
