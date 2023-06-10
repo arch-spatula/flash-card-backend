1f8b5ef27cbb7908e0da37c945d00776cf7e7d31
arch-spatula <shkim516@naver.com>
2023-06-10 18:21:40 +0900

    refactor: remove token class #42

--- util/token.ts
+++ util/token.ts
@@ -6,71 +6,6 @@ class TokenError extends Error {}
  * @see https://deno.land/x/djwt@v2.8
  */
 
-class Token {
-  private static instance: Token;
-  readonly key: Promise<CryptoKey>;
-
-  private constructor() {
-    this.key = (async () => {
-      const key = await crypto.subtle.generateKey(
-        { name: 'HMAC', hash: { name: 'SHA-512' } },
-        true,
-        ['sign', 'verify']
-      );
-      return key;
-    })();
-  }
-
-  static getInstance(): Token {
-    if (!Token.instance) {
-      Token.instance = new Token();
-    }
-    return Token.instance;
-  }
-
-  async makeAccessToken(userId: string, expiresInSec = 3600) {
-    const jwt = await create(
-      { alg: 'HS512' },
-      { exp: getNumericDate(expiresInSec), userId: userId },
-      await this.key
-    );
-    return {
-      jwt,
-      expires: new Date(new Date().getTime() + expiresInSec * 1000),
-    };
-  }
-
-  async makeRefreshToken(userId: string, expiresInSec = 2592000) {
-    const jwt = await create(
-      { alg: 'HS512' },
-      { exp: getNumericDate(expiresInSec), userId: userId },
-      await this.key
-    );
-    return {
-      jwt,
-      expires: new Date(new Date().getTime() + expiresInSec * 1000),
-    };
-  }
-
-  async refreshAccessToken(refreshToken: string) {
-    try {
-      const userId = await this.tokenToUserId(refreshToken);
-
-      if (!userId) throw new Error('토큰이 만료되었습니다.');
-
-      const { jwt: accessToken } = await this.makeAccessToken(userId);
-      return { accessToken, success: true };
-    } catch (_error) {
-      return { accessToken: null, success: false };
-    }
-  }
-
-  async tokenToUserId(jwt: string) {
-    const { userId } = await verify(jwt, await this.key, {});
-    return userId as string;
-  }
-}
-
 async function generateKey(): Promise<CryptoKey> {
   return await crypto.subtle.generateKey(
     { name: 'HMAC', hash: { name: 'SHA-512' } },
@@ -143,5 +78,3 @@ export {
   convertTokenToUserId,
   refreshAccessToken,
 };
-
-export default Token;
