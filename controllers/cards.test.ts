49ff6481817fee0195320d3e0cc5cbf63c7a9c93
arch-spatula <shkim516@naver.com>
2023-07-22 15:04:39 +0900

    test: add create, read happy path #29

--- controllers/cards.test.ts
+++ controllers/cards.test.ts
@@ -1,245 +1,45 @@
 import { Application, Router, assertEquals, superoak } from '../deps.ts';
-import { beforeEach } from '../deps.ts';
 import { addCard, deleteCard, getCards, updateCard } from './cards.ts';
-import type { Context } from '../deps.ts';
-import MongoAPI from '../api/mongoAPI.ts';
-import CardRecord from '../model/cards.ts';
 
 // import { Application, Router } from 'https://deno.land/x/oak@v10.4.0/mod.ts';
 
 const router = new Router();
 router
-  .get('/api/card', getCards)
-  .post('/api/card', addCard)
-  .patch('/api/card', updateCard)
-  .delete('/api/card', deleteCard);
+  .get('/api/card', (ctx) => {
+    ctx.state = { userId: 'user123' };
+    return getCards(ctx);
+  })
+  .post('/api/card', (ctx) => {
+    ctx.state = { userId: 'user123' };
+    return addCard(ctx);
+  })
+  .patch('/api/card/:id', (ctx) => {
+    ctx.state = { userId: 'user123' };
+    return updateCard(ctx);
+  })
+  .delete('/api/card/:id', (ctx) => {
+    ctx.state = { userId: 'user123' };
+    return deleteCard(ctx);
+  });
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-// Send simple GET request
-Deno.test('it should support the Oak framework', async () => {
-  const request = await superoak(app);
-  await request.get('/').expect('Hello Deno!');
-});
-
-// Custom requests can be built with the superagent API
-// https://visionmedia.github.io/superagent/#post--put-requests.
-Deno.test('it should allow post requests', async () => {
+Deno.test('it should create new card', async () => {
   const request = await superoak(app);
   await request
-    .post('/user')
-    .set('Content-Type', 'application/json')
-    .send('{"name":"superoak"}')
-    .expect(200)
-    .expect('Post!');
+    .post('/api/card')
+    .send({
+      question: 'CPU의 본딧말',
+      answer: 'Central Processing Unit',
+      submitDate: 'Wed May 17 2023 21:11:26 GMT+0900 (한국 표준시)',
+      stackCount: '0',
+    })
+    .expect(201);
 });
 
-// 테스트 코드를 작성할 때 필요한 모듈을 가져옵니다.
-// Mocking
-// class MockMongoAPI implements Partial<MongoAPI> {
-//   async getCards(userId: string) {
-//     return { success: true, userId };
-//   }
-
-//   async postCards(card: CardRecord) {
-//     return { success: true, card };
-//   }
-
-//   async patchCards(card: CardRecord) {
-//     return { success: true, card };
-//   }
-
-//   async deleteCards(id: string) {
-//     return { success: true, id };
-//   }
-// }
-
-// class MockToken implements Partial<Token> {
-//   async tokenToUserId(jwt: string) {
-//     if (jwt === 'validToken') {
-//       return 'userId';
-//     } else {
-//       return null;
-//     }
-//   }
-// }
-
-// // 테스트 코드를 그룹으로 묶을 수 있습니다.
-// Deno.test('API 테스트', () => {
-//   let response: Response;
-//   const mockMongoAPI = new MockMongoAPI();
-//   const mockToken = new MockToken();
-
-//   // 각 테스트 함수 실행 전에 실행되는 부분입니다.
-//   beforeEach(async () => {
-//     // response = { status: 0, body: null };
-//   });
-
-//   // 테스트 코드 예시 1: getCards
-//   Deno.test('getCards 테스트', async () => {
-//     const ctx: Context = {
-//       response,
-//       cookies: { get: () => Promise.resolve('validToken') },
-//     } as any;
-//     await getCards(ctx);
-
-//     assertEquals(response.status, 200);
-//     // assertEquals(response.body, { success: true, userId: 'userId' });
-//   });
-
-//   // 테스트 코드 예시 2: addCard
-//   Deno.test('addCard 테스트', async () => {
-//     const ctx: Context = {
-//       request: {
-//         hasBody: true,
-//         body: () =>
-//           Promise.resolve({
-//             value: {
-//               question: 'Question',
-//               answer: 'Answer',
-//               submitDate: '2023-05-25',
-//               stackCount: 5,
-//             },
-//           }),
-//       } as any,
-//       response,
-//       cookies: { get: () => Promise.resolve('validToken') },
-//     } as any;
-//     await addCard(ctx);
-
-//     assertEquals(response.status, 201);
-//     // assertEquals(response.body, {
-//     //   success: true,
-//     //   card: {
-//     //     question: 'Question',
-//     //     answer: 'Answer',
-//     //     submitDate: '2023-05-25',
-//     //     stackCount: 5,
-//     //     userId: 'userId',
-//     //   },
-//     // });
-//   });
-
-//   // 테스트 코드 예시 3: updateCard
-//   Deno.test('updateCard 테스트', async () => {
-//     const ctx: Context = {
-//       request: {
-//         hasBody: true,
-//         body: () =>
-//           Promise.resolve({
-//             value: {
-//               question: 'Updated Question',
-//               answer: 'Updated Answer',
-//               submitDate: '2023-05-26',
-//               stackCount: 10,
-//             },
-//           }),
-//       } as any,
-//       response,
-//       cookies: { get: () => Promise.resolve('validToken') },
-//     } as any;
-//     const helpers = { getQuery: () => ({ id: 'cardId' }) };
-//     // await updateCard({});
-
-//     assertEquals(response.status, 200);
-//     // assertEquals(response.body, {
-//     //   success: true,
-//     //   card: {
-//     //     question: 'Updated Question',
-//     //     answer: 'Updated Answer',
-//     //     submitDate: '2023-05-26',
-//     //     stackCount: 10,
-//     //     userId: 'userId',
-//     //     _id: 'cardId',
-//     //   },
-//     // });
-//   });
-
-//   // 테스트 코드 예시 4: deleteCard
-//   Deno.test('deleteCard 테스트', async () => {
-//     const ctx: Context = {
-//       response,
-//       cookies: { get: () => Promise.resolve('validToken') },
-//     } as any;
-//     const helpers = { getQuery: () => ({ id: 'cardId' }) };
-//     await deleteCard({ ...ctx, ...helpers });
-
-//     assertEquals(response.status, 204);
-//     assertEquals(response.body, null);
-//   });
-
-//   // 테스트 코드 예시 5: 인증이 안 된 경우
-//   Deno.test('인증되지 않은 사용자 테스트', async () => {
-//     const ctx: Context = {
-//       response,
-//       cookies: { get: () => Promise.resolve(null) },
-//     } as any;
-//     await getCards(ctx);
-
-//     assertEquals(response.status, 400);
-//     assertEquals(response.body, {
-//       success: false,
-//       msg: '인증이 안 되어 있습니다.',
-//     });
-//   });
-
-//   // 테스트 코드 예시 6: 요청 데이터가 부족한 경우
-//   Deno.test('요청 데이터 부족 테스트', async () => {
-//     const ctx: Context = {
-//       request: {
-//         hasBody: true,
-//         body: () =>
-//           Promise.resolve({
-//             value: {
-//               question: 'Question',
-//               answer: '',
-//               submitDate: '',
-//               stackCount: 5,
-//             },
-//           }),
-//       } as any,
-//       response,
-//       cookies: { get: () => Promise.resolve('validToken') },
-//     } as any;
-//     await addCard(ctx);
-
-//     assertEquals(response.status, 400);
-//     assertEquals(response.body, {
-//       success: false,
-//       msg: 'question, answer, data, stackCount 중 값이 1개 없습니다.',
-//     });
-//   });
-
-//   // 테스트 코드 예시 7: 토큰에서 사용자 ID를 가져올 수 없는 경우
-//   Deno.test('사용자 ID 없는 경우 테스트', async () => {
-//     const ctx: Context = {
-//       response,
-//       cookies: { get: () => Promise.resolve('invalidToken') },
-//     } as any;
-//     await getCards(ctx);
-
-//     assertEquals(response.status, 400);
-//     assertEquals(response.body, {
-//       // success: false,
-//       msg: '사용자 id가 없습니다.',
-//     });
-//   });
-
-//   // 테스트 코드 예시 8: 예외가 발생하는 경우
-//   Deno.test('예외 발생 테스트', async () => {
-//     const ctx: Context = {
-//       request: {
-//         hasBody: false,
-//         body: () => Promise.resolve({}),
-//       } as any,
-//       response,
-//       cookies: { get: () => Promise.resolve('validToken') },
-//     } as any;
-//     await addCard(ctx);
-
-//     assertEquals(response.status, 400);
-//     assertEquals(response.body, { success: false, msg: 'No Data' });
-//   });
-// });
+Deno.test('it should read new card', async () => {
+  const request = await superoak(app);
+  await request.get('/api/card').expect(200);
+});
